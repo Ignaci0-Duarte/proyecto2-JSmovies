29eab3ae67a58a67b5a7e1e316c2c239fd09cbac
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-03-06 19:52:37 -0300

    funcion para verificar que no estemos logueados, si estamos lo envia a 404

--- javascript/register.js
+++ javascript/register.js
@@ -1,3 +1,12 @@
+const verificarUsuarioActual = () => {
+  const usuarioActual = JSON.parse(localStorage.getItem("usuarioActual"));
+  if (usuarioActual) {
+    window.location.href = "https://rollingcodejsmovies.netlify.app/html/404";
+  }
+};
+
+verificarUsuarioActual();
+
 const inputUser = document.getElementById("idInputUsuario");
 const inputPass = document.getElementById("idInputContrasenia");
 const inputRPass = document.getElementById("idInputRContrasenia");
