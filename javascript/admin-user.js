ce269bd95dec5c79c7d40e28e728544d6ca915e0
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-03-06 22:45:53 -0300

    funcion cerrar sesion

--- javascript/admin-user.js
+++ javascript/admin-user.js
@@ -107,3 +107,22 @@ renderTable();
 inputUser && inputUser.addEventListener("input", formValues);
 inputRole && inputRole.addEventListener("input", formValues);
 buttonSendForm && buttonSendForm.addEventListener("click", verifyForm);
+
+const cerrarSesion = () => {
+  // Eliminar el usuarioActual del localStorage
+  localStorage.removeItem("usuarioActual");
+
+  // Obtener el array de usuarios del localStorage
+  const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+  // Verificar si existe el array de usuarios
+  if (usuarios) {
+    // Iterar sobre cada usuario y establecer el atributo "login" en false
+    usuarios.forEach((usuario) => {
+      usuario.login = false;
+    });
+
+    // Actualizar el array de usuarios en el localStorage
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  }
+};
