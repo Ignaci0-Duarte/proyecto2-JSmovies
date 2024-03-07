77cd8eebed202730c9734121e9809f02437d9975
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-03-06 22:54:19 -0300

    funcion cerrar sesion

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -83,3 +83,22 @@ function enviarMail() {
 inputUser.addEventListener("input", (event) => formValues(event));
 inputPass.addEventListener("input", (event) => formValues(event));
 buttonSendForm.addEventListener("click", eventClick);
+
+const cerrarSesion = () => {
+  // Eliminar el usuarioActual del localStorage
+  localStorage.removeItem("usuarioActual");
+
+  // Obtener el array de usuarios del localStorage
+  const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+  // Verificar si existe el array de usuarios
+  if (usuarios) {
+    // Iterar sobre cada usuario y establecer el atributo "login" en false
+    usuarios.forEach((usuario) => {
+      usuario.login = false;
+    });
+
+    // Actualizar el array de usuarios en el localStorage
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  }
+};
